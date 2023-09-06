f1622488ddebeb4b1ffcaedf4e61088e3547f4a2
Christudasan Devadasan <Christudasan.Devadasan@amd.com>
2023-09-06 19:42:57 +0530

    [AMDGPU] Introduce WWM_VGPR_32 regclass for wwm reg operands.

--- llvm/lib/Target/AMDGPU/SILowerWWMCopies.cpp
+++ llvm/lib/Target/AMDGPU/SILowerWWMCopies.cpp
@@ -55,7 +55,7 @@ private:
   SlotIndexes *Indexes;
   VirtRegMap *VRM;
   const SIRegisterInfo *TRI;
-  const MachineRegisterInfo *MRI;
+  MachineRegisterInfo *MRI;
   SIMachineFunctionInfo *MFI;
 };
 
@@ -108,9 +108,6 @@ bool SILowerWWMCopies::runOnMachineFunction(MachineFunction &MF) {
   TRI = ST.getRegisterInfo();
   MRI = &MF.getRegInfo();
 
-  if (!MFI->hasVRegFlags())
-    return false;
-
   bool Changed = false;
   for (MachineBasicBlock &MBB : MF) {
     for (MachineInstr &MI : MBB) {
@@ -137,5 +134,6 @@ bool SILowerWWMCopies::runOnMachineFunction(MachineFunction &MF) {
     }
   }
 
+  MRI->resetDynRegClasses();
   return Changed;
 }
